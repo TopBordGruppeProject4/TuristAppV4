7c90b28f8b1982aff8bf69010e22fa6221675258
Obelobel <nicolaioc@hotmail.dk>
2014-11-25 13:05:16 +0100

    Kategori

--- TursitAppV4/Model/Kategori.cs
+++ TursitAppV4/Model/Kategori.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace TursitAppV4.Model
 {
     class Kategori
     {
+        public String Genre { get; set; }
+        public ObservableCollection<Koncert> ListeAfKoncerter { get; set; }
+
     }
 }
