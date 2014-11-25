74da759a661259cb609fc8ad9eb8fd60d3aa0557
Obelobel <nicolaioc@hotmail.dk>
2014-11-25 11:22:00 +0100

    Bands og favoritter

--- TursitAppV4/Model/Bands.cs
+++ TursitAppV4/Model/Bands.cs
@@ -9,6 +9,17 @@ namespace TursitAppV4.Model
     class Bands
     {
         public string Navn { get; set; }
+        public string Beskrivelse { get; set; }
 
+        public Bands(string navn, string beskrivelse)
+        {
+            Navn = navn;
+            Beskrivelse = beskrivelse;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Navn: {0}, Beskrivelse: {1}", Navn, Beskrivelse);
+        }
     }
 }
--- TursitAppV4/Model/Farvoritter.cs
+++ TursitAppV4/Model/Farvoritter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace TursitAppV4.Model
 {
     class Farvoritter
     {
+        public ObservableCollection<Koncert> FavoritKoncerter { get; set; }
+
+        public Farvoritter(ObservableCollection<Koncert> favoritKoncerter)
+        {
+            FavoritKoncerter = favoritKoncerter;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("FavoritKoncerter: {0}", FavoritKoncerter);
+        }
+
+        public void AddKoncert()
+        {
+            
+        }
     }
+
+
 }
