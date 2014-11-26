421c579357633e360505bc923b20c317696bc1f4
Obelobel <nicolaioc@hotmail.dk>
2014-11-26 09:27:39 +0100

    Kategori og Koncert

--- TursitAppV4/Model/Koncert.cs
+++ TursitAppV4/Model/Koncert.cs
@@ -8,6 +8,22 @@ namespace TursitAppV4.Model
 {
     class Koncert
     {
+        public string Band { get; set; }
+        public string Tidspunkt { get; set; }
+        public string Dato { get; set; }
+        public string Spillested { get; set; }
 
+        public Koncert(string band, string tidspunkt, string dato, string spillested)
+        {
+            Band = band;
+            Tidspunkt = tidspunkt;
+            Dato = dato;
+            Spillested = spillested;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Band: {0}, Tidspunkt: {1}, Dato: {2}, Spillested: {3}", Band, Tidspunkt, Dato, Spillested);
+        }
     }
 }
