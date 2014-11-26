3f69eec04f8702d805cdd48911c6104db42d73bf
ProfessorCoke <Javer57@gmail.com>
2014-11-26 11:27:23 +0100

    TEGN OBEL
    please remember

--- TursitAppV4/Viewmodel/MainViewModel.cs
+++ TursitAppV4/Viewmodel/MainViewModel.cs
@@ -21,8 +21,8 @@ namespace TursitAppV4.Viewmodel
         static Band DeadMau5 = new Band("DeadMau5","placeholder");
         static Band VonDü = new Band("Von Dü","placeholder");
         static Band Redwarszawa = new Band("Red warszawa","placeholder");
-        
-        Kategori RockKategori = new Kategori("Rock","placeholder")
+
+        private Kategori RockKategori = new Kategori("Rock", "placeholder");
         Koncert DADkKoncert = new Koncert(DAD, "kl.20:00", "d.2-2-2020", Gimle);
         Koncert MagtensKorridorkKoncert = new Koncert(MagtensKorridor,"kl:19:00","d.2-02-2020",Paramount);
         
